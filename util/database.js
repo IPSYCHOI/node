9d942201bbf9f8df93857f8b204fa8330eecdbbc
psycho <aali571110@gmail.com>
2024-11-04 23:20:39 +0200

    final

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("psycho", "root", "psycho", {
-	dialect: "mysql",
-	host: "localhost",
-});
+// const sequelize = new Sequelize("psycho", "root", "psycho", {
+// 	dialect: "mysql",
+// 	host: "localhost",
+// });
+const sequelize = new Sequelize(
+	"sql12742662",
+	"sql12742662",
+	"8sw1yrenDa",
+	{
+		dialect: "mysql",
+		host: "sql12.freesqldatabase.com",
+		port: 3306,
+	}
+);
 
 module.exports = sequelize;
